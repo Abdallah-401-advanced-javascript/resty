e0cefac6256f27ee5c2743b8370e61829218325e
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-29 22:16:15 +0300

    test

--- resty-app/src/index.js
+++ resty-app/src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 class Main extends React.Component {
-    render() {
-        return <App />;
-    }
+  render() {
+    return <App />;
+  }
 }
 
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<Main />, rootElement);
