eb561557102d40566b5d0dcb1fa62a957126b1ea
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-29 20:49:47 +0300

    Updates

---
+++ precache-manifest.17d1794ce60f850b7639ae3e417ddc1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c1475ea95277db17de6bdd3eb2f3a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0c2612239203d638d4",
+    "url": "/static/css/main.d3656700.chunk.css"
+  },
+  {
+    "revision": "7f63f3fefb8ce8bd1c62",
+    "url": "/static/js/2.716d3eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.716d3eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0c2612239203d638d4",
+    "url": "/static/js/main.37abd699.chunk.js"
+  },
+  {
+    "revision": "411f183aa86445d317cd",
+    "url": "/static/js/runtime-main.8eb541cb.js"
+  }
+]);
