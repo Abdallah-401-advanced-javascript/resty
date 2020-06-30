21a3312754f98e3ca0e89461ad77af0fa652c605
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-30 21:04:58 +0300

    done

--- resty-app/components/thing/thing.js
+++ resty-app/components/thing/thing.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './thing.scss';
 
 class Thing extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {stuff: true};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {stuff: true};
+  }
 
     handlerClick = (e) => {
-        this.setState({stuff: !this.state.stuff});
-        console.log("this.state.stuff ===> ", this.state.stuff)
+      this.setState({stuff: !this.state.stuff});
+      console.log('this.state.stuff ===> ', this.state.stuff);
     }
 
     render() {
-        return (
-            <section>
-                <span>Stuff: {this.state.stuff.toString()}</span>
-                <button onClick={this.handlerClick}>Click</button>
-            </section>
-        )
+      return (
+        <section>
+          <span>Stuff: {this.state.stuff.toString()}</span>
+          <button onClick={this.handlerClick}>Click</button>
+        </section>
+      );
     }
 }
 
