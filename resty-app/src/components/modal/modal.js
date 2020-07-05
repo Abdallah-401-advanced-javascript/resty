24755d65364bf484fb95123d9ed73272468f6d86
Abdallah <eng.abdallahobaid@gmail.com>
2020-07-05 08:17:19 +0300

    done

--- resty-app/src/components/modal/modal.js
+++ resty-app/src/components/modal/modal.js
@@ -3,22 +3,22 @@ import './modal.scss';
 import React from 'react';
 
 class Modal extends React.Component {
-    render() {
+  render() {
 
-        return (
-            <div className="overlay">
-                <div className="modal">
-                    <div className="header">
-                        <span className="title">{this.props.title}</span>
-                        <button onClick={this.props.close}>X</button>
-                    </div>
-                    <div className="content">
-                        {this.props.children}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="overlay">
+        <div className="modal">
+          <div className="header">
+            <span className="title">{this.props.title}</span>
+            <button onClick={this.props.close}>X</button>
+          </div>
+          <div className="content">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Modal;
